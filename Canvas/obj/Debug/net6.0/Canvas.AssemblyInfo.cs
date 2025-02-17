a0bcecf9592680a532df97d2be7bd364f3b549cf
Schlafhase <schlafhase190@gmail.com>
2025-02-17 12:10:37 +0100

    Add ThreeBodyRenderer and GifWriter class (doesn't work)

--- Canvas/obj/Debug/net6.0/Canvas.AssemblyInfo.cs
+++ Canvas/obj/Debug/net6.0/Canvas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Canvas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669f414dbd129229b1e1d34633e392daa6e8263f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a665d78ad1125e7f2bd8c4a87c20f692f7c77244")]
 [assembly: System.Reflection.AssemblyProductAttribute("Canvas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Canvas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
